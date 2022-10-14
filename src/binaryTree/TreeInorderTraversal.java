fcbb856d42d1036313927590a0b406a98ffafb1a
asorokin <aleksandr.sorokin@extraenergy.com.cy>
2022-10-14 17:07:36 +0300

    Finished with the recursive method for inorder traversal of binary tree

--- src/binaryTree/TreeInorderTraversal.java
+++ src/binaryTree/TreeInorderTraversal.java
@@ -1,6 +1,6 @@
 package binaryTree;
 
-public class TreeInorderTravetsal {
+public class TreeInorderTraversal {
     /*
                    20
                /        \
@@ -14,7 +14,15 @@ public class TreeInorderTravetsal {
         TreeNode root = new TreeNode(20,
                 new TreeNode(7, new TreeNode(4, null, new TreeNode(6)), new TreeNode(9)),
                 new TreeNode(35, new TreeNode(31, new TreeNode(28), null), new TreeNode(40, new TreeNode(38), new TreeNode(52))));
-        System.out.println();
+        inorderTraversalRecursive(root);
+    }
+
+    private static void inorderTraversalRecursive(TreeNode node) {
+        if (node != null) {
+            inorderTraversalRecursive(node.left);
+            System.out.println(node.value);
+            inorderTraversalRecursive(node.right);
+        }
     }
 
 
